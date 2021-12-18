9d2a5d06fe1e37eb74e076d62a22dea59350fde5
beginnerDev <ruanthiago.in@gmail.com>
2021-12-18 16:26:50 -0300

    primeiro commit

---
+++ CoPainel2.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "13vw";
+  
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    
+    
+  }
+  var destiny = document.querySelector("dest1");
+  destiny.innerHTML("destination");
